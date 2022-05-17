83384a55c3031beca7c34bac8dd0149d8ddd2128
lsulecki <leiana.sulecki@gmail.com>
2022-05-17 19:59:43 -0400

    bar chart

--- charts.js
+++ charts.js
@@ -88,6 +88,7 @@ function buildCharts(sample) {
       y: yTicks,
       type: 'bar',
       text: hoverText,
+      orientation: 'h'
   }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
